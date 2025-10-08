2aad798c649ea2794a0708901c848c1cebf9f259
ElMolla10 <142741736+ElMolla10@users.noreply.github.com>
2025-10-09 02:00:36 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -61,7 +61,7 @@ MAX_NOTIONAL_PER_SYM = float(os.environ.get("MAX_NOTIONAL", "8000"))
 # Symbols
 TEST_MODE  = os.environ.get("TEST_MODE", "0") == "1"
 SYMBOLS    = [os.environ.get("TEST_SYMBOL", "AAPL")] if TEST_MODE else \
-             os.environ.get("SYMBOLS", "AAPL,MSFT,PG,AMD,JPM,XOM").split(",")
+             os.environ.get("SYMBOLS", "AAPL","MSFT","NVDA","AMD","JPM","GS","XOM","CVX","PG","KO").split(",")
 
 # Model & features
 MODEL_PATH = os.environ.get("MODEL_PATH", "app/model/XGboost_model.json")
