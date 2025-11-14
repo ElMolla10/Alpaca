feb2336d93de885658a6f8635c20bfee9ad6c94f
ElMolla10 <142741736+ElMolla10@users.noreply.github.com>
2025-11-14 12:58:42 +0200

    Add files via upload

---
+++ Website/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import { SignupForm } from "@/components/signup-form"
+
+export default function SignupPage() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-gradient-to-br from-slate-50 to-slate-100">
+      <SignupForm />
+    </main>
+  )
+}
