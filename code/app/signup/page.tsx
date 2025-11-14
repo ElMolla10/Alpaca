2e22ca1a614ab76264462bc3bc3a4f746f3a9a9b
ElMolla10 <142741736+ElMolla10@users.noreply.github.com>
2025-11-14 12:57:59 +0200

    Delete code directory

--- code/app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignupForm } from "@/components/signup-form"
-
-export default function SignupPage() {
-  return (
-    <main className="flex items-center justify-center min-h-screen bg-gradient-to-br from-slate-50 to-slate-100">
-      <SignupForm />
-    </main>
-  )
-}
